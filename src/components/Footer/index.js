0bae06af1934c7d5abb1acf184305b4fb881f647
Rahul Raj <rahul.raj@mindtickle.com>
2022-09-09 18:46:58 +0530

    added footer section

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,37 @@
 import React from "react";
+import { Typography } from "antd";
+import { GithubLogo, LinkedinLogo, TwitterLogo, ArrowUp } from "phosphor-react";
+
 import { StyledFooter } from "./style";
 
+const { Title, Link } = Typography;
+
 const Footer = () => {
-  return <StyledFooter></StyledFooter>;
+  return (
+    <StyledFooter>
+      <Title level={3} className="footer-title text-value-rt">
+        Designed and built by
+      </Title>
+      <div className="social-media-handle">
+        <Link href="https://github.com/notoveryet1308" target="_blank">
+          <GithubLogo className="rt-phosphor-icon" weight="fill" />
+        </Link>
+        <Link href="https://www.linkedin.com/in/rahul1308/" target="_blank">
+          <LinkedinLogo className="rt-phosphor-icon" weight="fill" />
+        </Link>
+        <Link href="https://twitter.com/rahulraz1308" target="_blank">
+          <TwitterLogo className="rt-phosphor-icon" weight="fill" />
+        </Link>
+      </div>
+      <div
+        className="go-to-top"
+        onClick={() => scroll({ top: 0, behavior: "smooth" })}
+      >
+        <ArrowUp className="rt-phosphor-icon" weight="fill" />
+        <span className="move-text">Move Up</span>
+      </div>
+    </StyledFooter>
+  );
 };
 
 export default Footer;
--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -4,6 +4,40 @@ import { Colors } from "../../styles/theme";
 export const StyledFooter = styled.div`
   position: relative;
   width: 100%;
-  height: 200px;
+  height: 150px;
   background-color: ${Colors.OFF_WHITE};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  row-gap: 20px;
+
+  .footer-title {
+  
+  }
+
+  .social-media-handle {
+    display: flex;
+    column-gap: 8px;
+  }
+  .rt-phosphor-icon {
+    font-size: 30px;
+  }
+  .go-to-top {
+    position: absolute;
+    right: 100px;
+    display: flex;
+    align-items: center;
+    column-gap: 8px;
+    cursor: pointer;
+    background-color: #000;
+    color: white;
+    padding: 4px 12px;
+    border-radius: 4px;
+    scroll-behavior: smooth;
+    .move-text {
+      font-size: 16px;
+      font-weight: 500;
+    }
+  }
 `;
